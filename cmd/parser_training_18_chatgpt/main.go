837e5d2d3ccc3a67b48518ce3ed4d8fe14be940b
Lokgaev <lokgaevm@gmail.com>
2026-08-28 16:30:27 +0500

    new commit

--- cmd/parser_training_18_chatgpt/main.go
+++ cmd/parser_training_18_chatgpt/main.go
@@ -2,36 +2,83 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
+type News struct {
+	Title string
+	URL   string
+	Text  string
+}
+
 func main() {
-	c := colly.NewCollector()
+	listCollector := colly.NewCollector()
+	articleCollector := colly.NewCollector()
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Отправляем запрос:", r.URL)
-	})
+	var newsList []News
 
-	c.OnHTML(`a[href^="/ru/news/"]`, func(e *colly.HTMLElement) {
+	var currentTitle string
+	var currentURL string
+	var currentText strings.Builder
+
+	listCollector.OnHTML(`a[href^="/ru/news/"]`, func(e *colly.HTMLElement) {
 		title := e.ChildText("h2")
-		link := e.Attr("href")
 
 		if title == "" {
 			return
 		}
 
-		fmt.Println("Заголовок:", title)
-		fmt.Println("Ссылка:", link)
+		link := e.Attr("href")
+		fullURL := e.Request.AbsoluteURL(link)
+
+		currentTitle = title
+		currentURL = fullURL
+
+		currentText.Reset()
+
+		err := articleCollector.Visit(fullURL)
+
+		if err != nil {
+			fmt.Println("Ошибка статьи:", err)
+		}
 	})
 
-	err := c.Visit("https://turkmenportal.com/ru/news?page=1")
+	articleCollector.OnHTML("#content p", func(e *colly.HTMLElement) {
+		text := strings.TrimSpace(e.Text)
+
+		if text == "" {
+			return
+		}
+
+		currentText.WriteString(text)
+		currentText.WriteString("\n")
+	})
+
+	articleCollector.OnScraped(func(r *colly.Response) {
+		news := News{
+			Title: currentTitle,
+			URL:   currentURL,
+			Text:  strings.TrimSpace(currentText.String()),
+		}
+
+		newsList = append(newsList, news)
+	})
+
+	err := listCollector.Visit("https://turkmenportal.com/ru/news?page=1")
 
 	if err != nil {
 		fmt.Println("Ошибка:", err)
 		return
 	}
 
+	fmt.Println("Количество новостей:", len(newsList))
+
+	for _, news := range newsList {
+		fmt.Println("Заголовок:", news.Title)
+		fmt.Println("URL:", news.URL)
+		fmt.Println("Текст:", news.Text)
+		fmt.Println("----------------")
+	}
 }
-fhdks := fmt.println whp are you
-fmt.printn("Who are you?")
